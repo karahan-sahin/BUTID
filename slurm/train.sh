992e7fc627b1d285715f81eed7396088ca573db7
OnurSefa <onursefa_ozcibik@hotmail.com>
2026-07-29 21:20:59 +0100

    exp update

--- slurm/train.sh
+++ slurm/train.sh
@@ -22,7 +22,7 @@ apptainer exec --nv \
   --pwd "${PROJECT_DIR}" \
   "${SIF_IMAGE}" \
   deepspeed --include localhost:0,1,2,3 --master_port 29511 train.py \
-  --batch-size 48 \
+  --batch-size 32 \
   --gradient-accumulation-steps 8 \
   --epochs 30 \
   --opt AdamW \
