760b9b93cd0341b49e13b20e70b1edeb2628ca02
Ozcibik <bj134lq@aisurrey-submit01.surrey.ac.uk>
2026-08-05 12:55:34 +0100

    exp

--- slurm/train.sh
+++ slurm/train.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name="BUTID SLT"
 #SBATCH --partition=cogvis-project
-#SBATCH --nodelist=aisurrey27
+#SBATCH --nodelist=aisurrey36
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=6
-#SBATCH --gres=gpu:6
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
 #SBATCH --mem-per-gpu=30G
 #SBATCH --time=03-00:00:00
 #SBATCH -o logs/slurm.%a.out
@@ -14,7 +14,7 @@
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
 PROJECT_DIR="/mnt/fast/nobackup/users/bj134lq/BUTID"
-SIF_IMAGE="${PROJECT_DIR}/docker/butid2.sif"
+SIF_IMAGE="${PROJECT_DIR}/docker/butid.sif"
 DATA_ROOT="/mnt/fast"
 
 apptainer exec --nv \
@@ -22,9 +22,9 @@ apptainer exec --nv \
   --bind "${DATA_ROOT}:${DATA_ROOT}" \
   --pwd "${PROJECT_DIR}" \
   "${SIF_IMAGE}" \
-  deepspeed --include localhost:0,1,2,3,4,5 --master_port 29511 train.py \
-  --batch-size 144 \
-  --gradient-accumulation-steps 4 \
+  deepspeed --include localhost:0 --master_port 29511 train.py \
+  --batch-size 256 \
+  --gradient-accumulation-steps 2 \
   --epochs 30 \
   --opt AdamW \
   --lr 3e-4 \
