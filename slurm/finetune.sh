306c224e36f0814bb017a8db24a5201a777daac0
Ozcibik <bj134lq@aisurrey-submit01.surrey.ac.uk>
2026-08-06 01:21:02 +0100

    exp

--- slurm/finetune.sh
+++ slurm/finetune.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-#SBATCH --job-name="BUTID SLT"
-#SBATCH --partition=cogvis-project
-#SBATCH --nodelist=aisurrey36
+#SBATCH --job-name="SLT 3"
+#SBATCH --partition=a100
+#SBATCH --nodelist=aisurrey24
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --gres=gpu:2
 #SBATCH --mem-per-gpu=30G
 #SBATCH --time=03-00:00:00
-#SBATCH -o logs/slurm.%a.out
-#SBATCH -e logs/slurm.%a.err
+#SBATCH -o logs/slurm3.%a.out
+#SBATCH -e logs/slurm3.%a.err
 
 echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
 PROJECT_DIR="/mnt/fast/nobackup/users/bj134lq/BUTID"
-SIF_IMAGE="${PROJECT_DIR}/docker/butid.sif"
+SIF_IMAGE="/mnt/fast/nobackup/users/bj134lq/BUTID/docker/butid2.sif"
 DATA_ROOT="/mnt/fast"
 
 apptainer exec --nv \
@@ -22,7 +22,7 @@ apptainer exec --nv \
   --bind "${DATA_ROOT}:${DATA_ROOT}" \
   --pwd "${PROJECT_DIR}" \
   "${SIF_IMAGE}" \
-  deepspeed --include localhost:0 --master_port 29511 train.py \
+  deepspeed --include localhost:0,1 --master_port 29511 train.py \
   --finetune /mnt/fast/nobackup/scratch4weeks/bj134lq/data/train_output/SLT/best_checkpoint.pth \
   --batch-size 256 \
   --gradient-accumulation-steps 1 \
@@ -30,7 +30,7 @@ apptainer exec --nv \
   --opt AdamW \
   --lr 2e-4 \
   --quick_break 2048 \
-  --output_dir /mnt/fast/nobackup/scratch4weeks/bj134lq/data/train_output2 \
+  --output_dir /mnt/fast/nobackup/scratch4weeks/bj134lq/data/train_output3 \
   --dataset BUTID \
   --tasks SLT \
   --csv-dir /mnt/fast/nobackup/users/bj134lq/BUTID/data/split \
